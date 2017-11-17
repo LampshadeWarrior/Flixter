48d6afa6e0c59a5226e9f5d3efa2b92b5163bbc9
Iann Singer <itpsinger@gmail.com>
2017-11-17 16:31:30 +0000

    Hooks up course creation forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
   resources :users, only: [:show]
+end
 end
